a323963911382e3e1814e33f08499cf619fe383d
rushu <rushurushu721@gmail.com>
2026-08-04 20:12:46 +0545

    register apis with jwt tokens updated

---
+++ backend/src/utils/cookie.js
@@ -0,0 +1,10 @@
+import {env} from "../../config/env.js";
+
+export const setRefreshTokenCookie = (res, token) => {
+    res.cookie("refreshToken", token, {
+        httpOnly: true,
+        secure: env.NODE_ENV === "production",
+        sameSite: "lax",
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 day
+    })
+}
--- backend/src/utils/jwt.js
+++ backend/src/utils/jwt.js
@@ -1,2 +1,23 @@
 import jwt from "jsonwebtoken";
+import { env } from "../../config/env.js";
 
+export const generateAccessToken = (payload) => {
+    return jwt.sign(payload,env.JWT_ACCESS_SECRET, {
+        expiresIn:env.ACCESS_TOKEN_EXPIRES
+
+    })
+}
+
+export const generateRefreshToken = (payload) => {
+    return jwt.sign(payload,env.JWT_REFRESH_SECRET, {
+        expiresIn:env.REFRESH_TOKEN_EXPIRES
+    })
+}
+
+export const verifyAccessToken = (payload) => {
+        return jwt.verify(token, env.JWT_ACCESS_SECRET);
+}
+
+export const verifyAccessToken = (payload) => {
+        return jwt.verify(token, env.JWT_REFRESH_SECRET);
+}
