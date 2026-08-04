69f4cbd09f61230fb981fb4bee50eedd42698d2c
rushu <rushurushu721@gmail.com>
2026-08-04 20:12:46 +0545

    register apis with jwt tokens updated

--- backend/src/module/auth/auth.service.js
+++ backend/src/module/auth/auth.service.js
@@ -1,44 +1,67 @@
-import {prisma} from "../../config/database.js";
+import { prisma } from "../../config/database.js";
 import { hashPassword } from "../../utils/hash.js";
 
-
 export const registerUser = async (userData) => {
-    const {fullName, email, phone, password, role} = userData;
-  
-    //email had existing phone or email 
-    const existingUser = await prisma.user.findFirst({
-        where: {
-            OR: [
-                { email},
-                ...email(phone?[{phone}]:[])
-            ]
-        }
-    })
-    if(existingUser){
-        throw new Error("User with this email or phone already exists")
-    }
-    const hashedPassword = await hashPassword(password)
-    const newUser = await prisma.user.create(
-      {
-        data:{
-            fullName,
-            email,
-            phone,
-            password: hashPassword,
-            role
+  const { fullName, email, phone, password, role } = userData;
 
-        },
-        select:{
-            id:true,
-            fullName:true,
-            email:true,
-            phone:true,
-            password:true,
-            role:true,
-            createdAt:true,
-            updatedAt:true
-        }
-       }
-    ) 
-    return newUser
-}
+  //email had existing phone or email
+  const existingUser = await prisma.user.findFirst({
+    where: {
+      OR: [{ email }, ...email(phone ? [{ phone }] : [])],
+    },
+  });
+  if (existingUser) {
+    throw new Error("User with this email or phone already exists");
+  }
+  const hashedPassword = await hashPassword(password);
+  const newUser = await prisma.user.create({
+    data: {
+      fullName,
+      email,
+      phone,
+      password: hashPassword,
+      role,
+    },
+  });
+
+  // select:{
+  //     id:true,
+  //     fullName:true,
+  //     email:true,
+  //     phone:true,
+  //     password:true,
+  //     role:true,
+  //     createdAt:true,
+  //     updatedAt:true
+  // }
+};
+const payload = {
+  id: user.id,
+  email: user.email,
+  role: user.role,
+};
+
+const accesstoken = generateAccessToken(payload);
+const refreshtoken = generateRefreshToken(payload);
+
+await prisma.user.update({
+  where: {
+    id: user.id,
+  },
+  data: {
+    refreshToken: refreshtoken,
+  },
+});
+
+return {
+  newUser: {
+    id: user.id,
+    fullName: user.fullName,
+    email: user.email,
+    phone: user.phone,
+    role: user.role,
+    isActive: user.isActive,
+  },
+  accessToken: accesstoken,
+  refreshToken: refreshtoken,
+};
