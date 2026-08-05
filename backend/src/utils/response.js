27e5e29893ae5735f38aacf89232f2279ab66b2c
rushu <rushurushu721@gmail.com>
2026-08-05 20:15:27 +0545

    constants and tokens

--- backend/src/utils/response.js
+++ backend/src/utils/response.js
@@ -1,6 +1,9 @@
-import { DatabaseSync } from "node:sqlite"
+import { STATUS_CODES } from "../constants/statusCodes.js"
+import { BAD_REQUEST } from "../constants/statusCodes.js"
+import { z } from "zod"
+import { INTERNAL_SERVER_ERROR } from "../constants/statusCodes.js"
 
-export const successResponse = (res, message, data =null, statusCode = 200) => {
+export const successResponse = (res, message, data =null, statusCode = STATUS_CODES.OK) => {
     return res.status(statusCode).json({
         success: true,
         message,
@@ -8,7 +11,7 @@ export const successResponse = (res, message, data =null, statusCode = 200) => {
     })
 }
 
-export const errorResponse =(res, message, errors=null,statusCode = 500) => {
+export const errorResponse =(res, message, errors=null,statusCode = INTERNAL_SERVER_ERROR) => {
     return res.status(statusCode).json({
         success: false,
         message,
