2d8022b61c0dc61be63bc193111923a37e9e7e11
rushu <rushurushu721@gmail.com>
2026-08-05 21:36:24 +0545

    access token and refresh tokens

--- backend/src/utils/response.js
+++ backend/src/utils/response.js
@@ -1,20 +1,28 @@
-import { STATUS_CODES } from "../constants/statusCodes.js"
-import { BAD_REQUEST } from "../constants/statusCodes.js"
-import { z } from "zod"
-import { INTERNAL_SERVER_ERROR } from "../constants/statusCodes.js"
+import { STATUS_CODES } from "../constans/statusCodes.js";
 
-export const successResponse = (res, message, data =null, statusCode = STATUS_CODES.OK) => {
-    return res.status(statusCode).json({
-        success: true,
-        message,
-        data
-    })
-}
+export const successResponse = (
+  res,
+  message,
+  data = null,
+  statusCode = STATUS_CODES.OK
+) => {
+  return res.status(statusCode).json({
+    success: true,
+    message,
+    data,
+  });
+};
 
-export const errorResponse =(res, message, errors=null,statusCode = INTERNAL_SERVER_ERROR) => {
-    return res.status(statusCode).json({
-        success: false,
-        message,
-        errors
-    })
-}
+
+export const errorResponse = (
+  res,
+  message,
+  errors = null,
+  statusCode = STATUS_CODES.INTERNAL_SERVER_ERROR
+) => {
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    errors,
+  });
+};
