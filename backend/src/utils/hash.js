4ccc17ec8a1e4e8d99b0bac3f125f3f059e90cdc
rushu <rushurushu721@gmail.com>
2026-08-08 11:37:27 +0545

    authorization middleware amd validation middleware

--- backend/src/utils/hash.js
+++ backend/src/utils/hash.js
@@ -1,24 +1,24 @@
-import jwt from "jsonwebtoken" 
-import { env } from "../config/env.js"
+import jwt from "jsonwebtoken";
+import { env } from "../config/env.js";
 
+export const generateAccessToken = (payload) => {
+  return jwt.sign(payload, env.JWT_ACCESS_SECRET, {
+    expiresIn: env.ACCESS_TOKEN_EXPIRES,
+  });
+};
 
-export  const generateAccessToken=(payload)=>{
-   return jwt.sign(payload,env.JWT_ACCESS_SECRET,{
-         expiresIn:env.ACCESS_TOKEN_EXPIRES
-    })
-}
+export const generateRefreshToken = (payload) => {
+  return jwt.sign(payload, env.JWT_REFRESH_SECRET, {
+    expiresIn: env.REFRESH_TOKEN_EXPIRES,
+  });
+};
 
+export const verifyAccessToken = (token) => {
+  return jwt.verify(token, env.JWT_ACCESS_SECRET);
+};
 
-export const generateRefreshToken=(payload)=>{
-    return jwt.sign(payload,env.JWT_REFRESH_SECRET,{
-         expiresIn:env.REFRESH_TOKEN_EXPIRES
-    })
-}
+export const verifyRefreshToken = (token) => {
+  return jwt.verify(token, env.JWT_REFRESH_SECRET);
+};
 
-export const verifyACCESSTOKEN=(payload)=>{
-    return jwt.verify(token , env.JWT_ACCESS_SECRET)
-}
-
-export const verifyRefreshToken=(payload)=>{
-    return jwt.verify(token , env.JWT_REFRESH_SECRET)
-}
+export const verifyACCESSTOKEN = verifyAccessToken;
